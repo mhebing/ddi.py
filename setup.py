822a188d2973d441f93a29fbfee30f4ba7447edc
mhebing <marcel@ddionrails.org>
2016-02-29 09:00:39 +0100

    More specific version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="ddi",
-    version="0.1",
+    version="0.1.0.dev1",
     description="DDI related tools",
     url="http://ddionrails.org",
     author="Marcel Hebing",
