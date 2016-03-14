9beb286403ad59ccc4b0793720965b7e9b12e73b
mhebing <marcel@ddionrails.org>
2016-03-14 16:04:28 +0100

    lxml works in ipython but breaks the unit tests

--- ddi/ddi.py
+++ ddi/ddi.py
@@ -1,3 +1,4 @@
+from lxml.builder import ET
 import pandas as pd
 
 class DDI:
@@ -16,12 +17,27 @@ class DDI:
                 self._add_frequencies(var, varname, meta)
                 self._add_basic_statistics(var, varname, meta)
 
+    def as_xml(self):
+        doc = ET.Element("codebook")
+        for varname, meta in self.meta.items():
+            var = ET.SubElement(doc, "var")
+            name = ET.SubElement(var, "name")
+            name.text = meta["name"]
+        return doc
+
     def _add_frequencies(self, var, varname, meta):
         if len(var.unique()) < 30:
             counts = dict(var.value_counts(
                 dropna=False,
                 sort=False
             ))
+            for x in counts.keys():
+                e = counts.pop(x)
+                try:
+                    x = int(x)
+                except:
+                    pass
+                counts[x] = e
             meta["frequencies"] = counts
 
     def _add_basic_statistics(self, var, varname, meta):
--- itest.py
+++ itest.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from lxml.builder import ET
 from ddi import ddi, statareader
 
 test1 = statareader.read_stata("test/data/test1.dta")
