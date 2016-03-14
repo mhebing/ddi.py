d387e7ba7c6fc6c1d0b60466af8b8f5fd6632461
mhebing <marcel@ddionrails.org>
2016-03-14 15:26:23 +0100

    Add basic statistics

--- ddi/ddi.py
+++ ddi/ddi.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 class DDI:
     """
     Base DDI class.
@@ -12,6 +14,7 @@ class DDI:
             if varname in self.data:
                 var = self.data[varname]
                 self._add_frequencies(var, varname, meta)
+                self._add_basic_statistics(var, varname, meta)
 
     def _add_frequencies(self, var, varname, meta):
         if len(var.unique()) < 30:
@@ -20,3 +23,17 @@ class DDI:
                 sort=False
             ))
             meta["frequencies"] = counts
+
+    def _add_basic_statistics(self, var, varname, meta):
+        statistics = dict(
+            count = len(var),
+            missing_cases = sum(pd.isnull(var)),
+        )
+        statistics["valid_cases"] = statistics["count"] - statistics["missing_cases"]
+        try:
+            statistics["var"] = var.var()
+            statistics["min"] = var.min()
+            statistics["max"] = var.max()
+        except:
+            pass
+        meta["statistics"] = statistics
