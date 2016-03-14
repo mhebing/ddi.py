05321e81b652f453709044b4f42d6ef8252ac196
mhebing <marcel@ddionrails.org>
2016-03-14 17:12:36 +0100

    Paver tasks for requirements and freeze

--- pavement.py
+++ pavement.py
@@ -13,3 +13,19 @@ def test():
     Run tests using unittest disccover.
     """
     run("python -m unittest discover")
+
+@task
+def setup_virtualenv():
+    """Setup virtualenv in: ~/.envs/data/"""
+    run("""
+        mkdir -p ~/.envs
+        virtualenv -p /usr/bin/python3.4 ~/.envs/data
+        source ~/.envs/data/bin/activate
+        pip install -r requirements.txt
+    """)
+
+@task
+def freeze_requirements():
+    """Freeze requirements."""
+    run("pip freeze > requirements.txt")
+
