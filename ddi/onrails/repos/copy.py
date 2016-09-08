43703d0d54ccdd49ff8f376b442e32f1b0a599f7
Marcel Hebing <marcel@ddionrails.org>
2016-09-08 15:46:32 +0200

    Add copy function for bibtex

--- ddi/onrails/repos/copy.py
+++ ddi/onrails/repos/copy.py
@@ -4,6 +4,9 @@ import pandas as pd
 def study():
     os.system("cp metadata/study.md ddionrails")
 
+def bibtex():
+    os.system("cp metadata/bibtex.bib ddionrails")
+
 def r2ddi(in_path, out_path):
     os.system("""
         mkdir -p ddionrails/r2ddi/v2013
