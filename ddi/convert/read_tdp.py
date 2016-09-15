ec1d26d9cb006cf77461d54b7a55549f98316ac0
Marcel Hebing <marcel@ddionrails.org>
2016-09-15 14:53:07 +0200

    Scaffold new convert functions

---
+++ ddi/convert/read_tdp.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def read_tdp(csv_file_name, json_file_name)
+   # hier passiert was
+   d = pd.DataFrame()
+   m = dict()
+   return d, m
---
+++ ddi/convert/write_stats.py
@@ -0,0 +1,11 @@
+import json
+import pandas as pd
+
+def write_stats(data, metadata, filename, file_type="json"):
+    if file_type == "json":
+        pass
+    elif file_type == "yaml":
+        pass
+    else
+        print("[ERROR] Unknown file type.")
+
