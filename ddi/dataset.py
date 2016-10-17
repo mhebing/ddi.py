f628b7d9832958f7ce00db9d82e2dd4ad50de8ea
Marius Pahl <mpahl@diw.de>
2016-10-17 22:38:03 +0200

    add all functions from convert

--- ddi/dataset.py
+++ ddi/dataset.py
@@ -12,6 +12,15 @@ class Dataset:
         
     def read_tdp(self, csv_name, json_name):
         self.dataset, self.metadata = read_tdp(csv_name, json_name)
+        
+    def read_stata(self, dta_name):
+        self.dataset, self.metadata = read_stata(dta_name)
     
-    def write_stats(self, json_name, file_type="json"):
-        write_stats(self.dataset, self.metadata, json_name, file_type=file_type)
+    def write_stats(self, output_name, file_type="json"):
+        write_stats(self.dataset, self.metadata, output_name, file_type=file_type)
+        
+    def write_tdp(self, output_csv, output_json):
+        write_tdp(self.dataset, self.metadata, output_csv, output_json)
+        
+    def write_stata(self, output_name):
+        write_stata(self.dataset, self.metadata, output_name)
