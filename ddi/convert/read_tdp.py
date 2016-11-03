c57a1eda790de7d5d1b4a2bda5e673bfc36ff699
Marius Pahl <mpahl@diw.de>
2016-11-03 13:05:41 +0100

    update

--- ddi/convert/read_tdp.py
+++ ddi/convert/read_tdp.py
@@ -7,7 +7,10 @@ def read_tdp(csv_file_name, json_file_name):
     print("read \"" + csv_file_name + "\" and \"" + json_file_name + "\"")
     d = pd.read_csv(csv_file_name)
     #replace all stata missings (. and .a etc.) with NaN
-    d = d.replace({"^\.\D?$":np.nan}, regex=True)
+    try:
+        d = d.replace({"^\.\D?$":np.nan}, regex=True)
+    except:
+        pass
     with open(json_file_name) as json_file:
         metadata = json_file.read()
     m = json.loads(metadata)
