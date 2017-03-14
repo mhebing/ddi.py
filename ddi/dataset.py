6356d6102d8fc7bafd69f7a2d5efc65693dad38a
Marius Pahl <mpahl@diw.de>
2017-03-14 18:28:04 +0100

    add description in class Dataset

--- ddi/dataset.py
+++ ddi/dataset.py
@@ -8,13 +8,15 @@ import re
 
 class Dataset:
     """
-    TODO Documentation of the entire class including example
+    Dataset allows the user to read, test and export data in different formats.
     
-    Example::
+    Example:
         
         dataset = Dataset()
-        dataset.read_stata("../data/dataset.dta")
+        dataset.read_stata("../input/dataset.dta")
+        dataset.test()
         dataset.write_stats("../output/dataset.json")
+        dataset.write_tdp("../output/dataset
     """
 
     def __init__(self):
@@ -22,21 +24,96 @@ class Dataset:
         self.metadata = None
         
     def read_tdp(self, csv_name, json_name):
+        '''
+        Function to read data in tabular data package format.
+        
+        Parameter:
+        
+        csv_name: Name of the row data in tabular format
+        json_name: Name of the metadata in json format
+        
+        Example:
+        
+        dataset.read_tdp("../input/dataset.csv", "../input/dataset.json")
+        '''
         self.dataset, self.metadata = read_tdp(csv_name, json_name)
         
     def read_stata(self, dta_name):
+        '''
+        Function to read data in stata format.
+        
+        Parameter:
+        
+        dta_name: Name of the data in stata format
+        
+        Example:
+        
+        dataset.read_stata("../input/dataset.dta")        
+        '''
         self.dataset, self.metadata = read_stata(dta_name)
     
     def write_stats(self, output_name, file_type="json", split="", weight=""):
+        '''
+        Function to write statistics from data in json/html format.
+        
+        Parameter:
+        
+        output_name: Name of the output file
+        file_type: Statistics are read out in json or html; Standard is "json"
+        split: Name of the variable(s) for bivariate statistics; Standard is ""
+        weight: Name of the weight variable; Standard is ""
+        
+        Example:
+        
+        dataset.write_stats("../output/dataset.html", file_type="html", split="split", weight="weight") 
+        ''' 
         write_stats(self.dataset, self.metadata, output_name, file_type=file_type, split=split, weight=weight)
         
     def write_tdp(self, output_csv, output_json):
+        '''
+        Function to write data in tdp format.
+        
+        Parameter:
+        
+        output_csv: Name of the row data in tabular format
+        output_json: Name of the metadata in json format
+        
+        Example:
+        
+        dataset.write_tdp("../output/dataset.csv", "../output/dataset.json") 
+        '''
         write_tdp(self.dataset, self.metadata, output_csv, output_json)
         
     def write_stata(self, output_name):
+        '''
+        Function to write data in stata format.
+        
+        Parameter:
+        
+        output_name: Name of the data in stata format
+        
+        Example:
+        
+        dataset.write_stata("../output/dataset.dta") 
+        '''
         write_stata(self.dataset, self.metadata, output_name)
         
     def test(self):
+        '''
+        Function to test data:
+        completeness of the metadata
+        correctness of unique values (i.e. id)
+        correctness of range (i.e. age)
+        Checking if the number of values of categorical variables in the data is equal to the number in the metadata
+        
+        Parameter:
+        
+        none
+        
+        Example:
+        
+        dataset.test()
+        '''
         for func in dir(test_values):
             if(re.search("^test_.*", func)!=None):
                 func = getattr(test_values, func)
