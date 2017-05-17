2c2ccfa5adfd2ca2193854c2f0ef3b1c347fddf1
Marcel Hebing <marcel@ddionrails.org>
2017-05-17 16:51:15 +0200

    Focus stata_to_statistics: only export statistics

--- ddi/convert/stata_to_statistics.py
+++ ddi/convert/stata_to_statistics.py
@@ -9,12 +9,18 @@ from ddi.dataset import Dataset
 def stata_to_statistics(study_name, input_csv, input_path, output_path):
     filereader = pd.read_csv(input_csv, delimiter=",", header = 0)
 
-    for data, weight, split, analysis_unit, period, sub_type in zip(filereader.filename, filereader.weight, filereader.split, filereader.analysis_unit, filereader.period, filereader.sub_type):
+    for data, weight, split, analysis_unit, period, sub_type in zip(
+        filereader.filename, filereader.weight, filereader.split,
+        filereader.analysis_unit, filereader.period, filereader.sub_type
+    ):
         d1 = Dataset()
         try:
             d1.read_stata(input_path + data + ".dta")
         except:
             print("Unable to find " + data + ".dta.")
             continue
-        d1.write_tdp(output_path + data + ".csv", output_path + data + ".json")
-        d1.write_stats(output_path + data + "_stats.json", split=split, weight=weight, analysis_unit=analysis_unit, period=period, sub_type=sub_type, study=study_name)
+        d1.write_stats(
+            output_path + data + "_stats.json", split=split, weight=weight,
+            analysis_unit=analysis_unit, period=period, sub_type=sub_type,
+            study=study_name
+        )
