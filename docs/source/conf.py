e8b940c87070fa60ccc73db49c71cca36ed5a47e
mhebing <marcel.hebing@gmail.com>
2018-11-02 13:23:57 +0100

    Start working on Pipfile and prepare automodule docu for ddi.onrails.repos

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,6 +16,8 @@ import os
 import sys
 # sys.path.insert(0, os.path.abspath('../..'))
 sys.path.append(os.path.join(os.path.dirname(__name__), '..'))
+sys.path.insert(0, os.path.abspath("../ddi"))
+sys.path.insert(0, os.path.abspath("../../ddi"))
 
 autodoc_mock_imports = ['ddi.convert.read_tdp', 'convert.read_stata', 'convert.write_stats', 'convert.write_tdp', 'convert.write_stata', 'test_values']
 
--- docs/source/ddi/onrails/index.rst
+++ docs/source/ddi/onrails/index.rst
@@ -3,3 +3,7 @@ onrails
 
 ddi/onrails contains a couple of modules to support data managers in preparing
 their metadata for the import in DDI on Rails.
+
+.. automodule:: ddi.onrails.repos.topics
+    :members:
+    :undoc-members:
