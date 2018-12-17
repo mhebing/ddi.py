b3a9fd676578dab44022c89e193051c88494f89b
afuetterer <fuetterh@posteo.de>
2018-12-17 14:59:28 +0100

    Black and isort

--- ddi/convert/read_tdp.py
+++ ddi/convert/read_tdp.py
@@ -1,17 +1,18 @@
-import re, os
-import json, yaml
+import json
+import logging
+
 import numpy as np
 import pandas as pd
-import logging
 
 logger = logging.getLogger(__name__)
 
+
 def read_tdp(csv_file_name, json_file_name):
-    logger.info("read \"" + csv_file_name + "\" and \"" + json_file_name + "\"")
+    logger.info('read "' + csv_file_name + '" and "' + json_file_name + '"')
     d = pd.read_csv(csv_file_name, index_col=None)
-    #replace all stata missings (. and .a etc.) with NaN
+    # replace all stata missings (. and .a etc.) with NaN
     try:
-        d = d.replace({"^\.\D?$":np.nan}, regex=True)
+        d = d.replace({"^\.\D?$": np.nan}, regex=True)
     except:
         pass
     with open(json_file_name) as json_file:
