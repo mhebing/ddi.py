507868fb914feb042677bbe879fd1a508e9e94e0
afuetterer <fuetterh@posteo.de>
2018-12-17 15:00:23 +0100

    Black and isort

--- ddi/convert/stata_to_statistics.py
+++ ddi/convert/stata_to_statistics.py
@@ -1,20 +1,27 @@
-import re
-import pandas as pd
-import numpy as np
-import os,sys, yaml, csv
 import logging
+import os
+import sys
+
+import pandas as pd
+from ddi.dataset import Dataset
 
 logger = logging.getLogger(__name__)
 
 sys.path.append(os.path.abspath("../../../ddi.py"))
-from ddi.dataset import Dataset
 
-def stata_to_statistics(study_name, input_csv, input_path, output_path, input_path_de=""):
-    filereader = pd.read_csv(input_csv, delimiter=",", header = 0)
+
+def stata_to_statistics(
+    study_name, input_csv, input_path, output_path, input_path_de=""
+):
+    filereader = pd.read_csv(input_csv, delimiter=",", header=0)
 
     for data, weight, split, analysis_unit, period, sub_type in zip(
-        filereader.filename, filereader.weight, filereader.split,
-        filereader.analysis_unit, filereader.period, filereader.sub_type
+        filereader.filename,
+        filereader.weight,
+        filereader.split,
+        filereader.analysis_unit,
+        filereader.period,
+        filereader.sub_type,
     ):
         d1 = Dataset()
         try:
@@ -22,19 +29,26 @@ def stata_to_statistics(study_name, input_csv, input_path, output_path, input_pa
         except:
             logger.warning("Unable to find " + data + ".dta in " + input_path + ".")
             continue
-        
-        metadata_de=""
+
+        metadata_de = ""
         if input_path_de != "":
             d2 = Dataset()
             try:
                 d2.read_stata(input_path_de + data + ".dta")
-                metadata_de=d2.metadata
+                metadata_de = d2.metadata
             except:
-                logger.warning("Unable to find " + data + ".dta in " + input_path_de + ".")
+                logger.warning(
+                    "Unable to find " + data + ".dta in " + input_path_de + "."
+                )
                 continue
 
         d1.write_stats(
-            output_path + data + "_stats.json", split=split, weight=weight,
-            analysis_unit=analysis_unit, period=period, sub_type=sub_type,
-            study=study_name, metadata_de=metadata_de
+            output_path + data + "_stats.json",
+            split=split,
+            weight=weight,
+            analysis_unit=analysis_unit,
+            period=period,
+            sub_type=sub_type,
+            study=study_name,
+            metadata_de=metadata_de,
         )
