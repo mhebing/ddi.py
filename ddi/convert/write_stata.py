bdc5aecd504ff38207244b538924314f0f219091
afuetterer <fuetterh@posteo.de>
2018-12-17 15:01:25 +0100

    Black and isort

--- ddi/convert/write_stata.py
+++ ddi/convert/write_stata.py
@@ -1,9 +1,8 @@
-from jinja2 import Template
-import os, sys
-import re
-import pandas as pd
-import numpy as np
 import logging
+import os
+import re
+
+from jinja2 import Template
 
 logger = logging.getLogger(__name__)
 
@@ -27,21 +26,23 @@ label values {{ x["name"].lower() }} {{ x["name"].lower() }}_label
 {% endfor %}
 """
 
+
 def save_do(output_do, do):
 
-    logger.info("write \"" + output_do + "\"")
+    logger.info('write "' + output_do + '"')
     with open(os.path.join(output_do), "w") as field1:
         field1.write(do)
+
 
 def generate_do(data_name, file_csv, file_json):
     template = Template(template_do)
     meta = template.render(
-        input_list=file_json["resources"][0]["schema"]["fields"],
-        data_name=data_name,
+        input_list=file_json["resources"][0]["schema"]["fields"], data_name=data_name
     )
     return meta
-    
+
+
 def write_stata(d, m, output_do):
-    data_name = re.sub(".do", ".csv", re.search('^.*\/(.*)', output_do).group(1))
+    data_name = re.sub(".do", ".csv", re.search("^.*\/(.*)", output_do).group(1))
     do = generate_do(data_name, d, m)
     save_do(output_do, do)
